6f178a2e7ba2ce737940e5faf3fd193bc4315238
dv-script <davihasson@gmail.com>
2024-07-28 12:34:27 -0300

    feat: new authenticate route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { register } from "@/http/controllers/register.controller";
+import { authenticate } from "./controllers/authenticate.controller";
 
 export default async function routes(app: FastifyInstance) {
   app.post("/users", register);
+  app.post("/auth", authenticate);
 }
