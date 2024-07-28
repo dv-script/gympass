059ad568d6c5207ba349e4e6a5f23e292c177b80
dv-script <davihasson@gmail.com>
2024-07-28 10:55:16 -0300

    feat: returning user as an object and linting the code

--- src/use-cases/register.usecase.ts
+++ src/use-cases/register.usecase.ts
@@ -1,6 +1,7 @@
 import { UserRepository } from "@/repositories/user.repository";
 import { hash } from "bcrypt";
-import { UserAlreadyExists } from "./errors/user-already-exists.error";
+import { UserAlreadyExistsError } from "./errors/user-already-exists.error";
+import { User } from "@prisma/client";
 
 interface RegisterUseCaseRequest {
   email: string;
@@ -8,21 +9,31 @@ interface RegisterUseCaseRequest {
   password: string;
 }
 
+interface RegisterUseCaseResponse {
+  user: User;
+}
+
 export class RegisterUseCase {
   constructor(private userRepository: UserRepository) {}
 
-  async execute({ email, name, password }: RegisterUseCaseRequest) {
+  async execute({
+    email,
+    name,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const userWithSameEmail = await this.userRepository.findByEmail(email);
 
     if (userWithSameEmail) {
-      throw new UserAlreadyExists();
+      throw new UserAlreadyExistsError();
     }
 
     const password_hash = await hash(password, 6);
-    await this.userRepository.create({
+    const user = await this.userRepository.create({
       email,
       name,
       password_hash,
     });
+
+    return { user };
   }
 }
