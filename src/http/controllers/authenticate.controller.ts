72eaec6f0545ee4767d3210e59ad2a0ec5787bb3
dv-script <davihasson@gmail.com>
2024-07-28 12:34:51 -0300

    feat: new authenticate controller

---
+++ src/http/controllers/authenticate.controller.ts
@@ -0,0 +1,58 @@
+import { z } from "zod";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { PrismaUserRepository } from "@/repositories/prisma/prisma-user.repository";
+import { AuthenticateUseCase } from "@/use-cases/authenticate.usecase";
+import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials.error";
+import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate.usecase";
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const authenticateUserSchema = z.object({
+    email: z.string().email({ message: "Invalid e-mail address" }),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long" })
+      .regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*]).*$/, {
+        message: "Password must contain at least:",
+      })
+      .regex(/^(?=.*[A-Z]).*$/, {
+        message: "- 1 Uppercase letter",
+      })
+      .regex(/^(?=.*[a-z]).*$/, {
+        message: "- 1 Lowercase letter",
+      })
+      .regex(/^(?=.*[0-9]).*$/, {
+        message: "- 1 Number",
+      })
+      .regex(/^(?=.*[!@#$%^&*]).*$/, {
+        message: "- 1 Special character",
+      }),
+  });
+
+  const validatedData = authenticateUserSchema.safeParse(request.body);
+
+  if (!validatedData.success) {
+    return reply.status(400).send(validatedData.error.errors);
+  }
+
+  const { email, password } = validatedData.data;
+
+  try {
+    const authenticateUseCase = makeAuthenticateUseCase();
+
+    await authenticateUseCase.execute({
+      email,
+      password,
+    });
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send(error.message);
+    }
+
+    throw error;
+  }
+
+  return reply.status(200).send();
+}
