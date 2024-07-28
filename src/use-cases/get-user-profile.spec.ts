af89fa08ae3df4a85131c37c72d930400fbec927
dv-script <davihasson@gmail.com>
2024-07-28 12:37:32 -0300

    feat: new get user profile use case

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,37 @@
+import { hash } from "bcrypt";
+import { UserInMemoryRepository } from "@/repositories/in-memory/in-memory-user.repository";
+import { expect, describe, it, beforeEach } from "vitest";
+import { GetUserProfileUseCase } from "./get-user-profile.usecase";
+import { ResourceNotFoundError } from "./errors/resource-not-found.error";
+
+let usersRepository: UserInMemoryRepository;
+let sut: GetUserProfileUseCase;
+
+describe("Get User Profile Use Case", () => {
+  beforeEach(() => {
+    usersRepository = new UserInMemoryRepository();
+    sut = new GetUserProfileUseCase(usersRepository);
+  });
+
+  it("should be able to get user profile", async () => {
+    const createdUser = await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    });
+
+    expect(user.name).toEqual("John Doe");
+  });
+
+  it("should not be able to get user profile with wrong id", async () => {
+    expect(() =>
+      sut.execute({
+        userId: "non-existing-id",
+      })
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+});
---
+++ src/use-cases/get-user-profile.usecase.ts
@@ -0,0 +1,29 @@
+import { UserRepository } from "@/repositories/user.repository";
+import { InvalidCredentialsError } from "./errors/invalid-credentials.error";
+import { compare } from "bcrypt";
+import { User } from "@prisma/client";
+import { ResourceNotFoundError } from "./errors/resource-not-found.error";
+
+interface GetUserProfileUseCaseRequest {
+  userId: string;
+}
+
+interface GetUserProfileUseCaseResponse {
+  user: User;
+}
+
+export class GetUserProfileUseCase {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute({
+    userId,
+  }: GetUserProfileUseCaseRequest): Promise<GetUserProfileUseCaseResponse> {
+    const user = await this.userRepository.findById(userId);
+
+    if (!user) {
+      throw new ResourceNotFoundError();
+    } 
+
+    return { user };
+  }
+}
