53c4b1384162b43906e2e5b9297c3895cbd4cda2
dv-script <davihasson@gmail.com>
2024-07-29 07:50:24 -0300

    feat: adding validation for maximum distance and checking if it's the only check in on the day

--- src/use-cases/check-in.usecase.ts
+++ src/use-cases/check-in.usecase.ts
@@ -1,9 +1,16 @@
 import { CheckIn } from "@prisma/client";
 import { CheckInRepository } from "@/repositories/check-in.repository";
+import { GymRepository } from "@/repositories/gym.repository";
+import { ResourceNotFoundError } from "./errors/resource-not-found.error";
+import { getDistanceInKilometersBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
+import { CheckInOnSameDayError } from "./errors/check-in-on-same-day.error";
+import { CheckInDistanceError } from "./errors/check-in-distance.error";
 
 interface CheckInUseCaseRequest {
   userId: string;
   gymId: string;
+  userLatitude: number;
+  userLongitude: number;
 }
 
 interface CheckInUseCaseResponse {
@@ -11,12 +18,46 @@ interface CheckInUseCaseResponse {
 }
 
 export class CheckInUseCase {
-  constructor(private checkInRepository: CheckInRepository) {}
+  constructor(
+    private checkInRepository: CheckInRepository,
+    private gymRepository: GymRepository
+  ) {}
 
   async execute({
     userId,
     gymId,
+    userLatitude,
+    userLongitude,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const gym = await this.gymRepository.findById(gymId);
+
+    if (!gym) {
+      throw new ResourceNotFoundError();
+    }
+
+    const distance = getDistanceInKilometersBetweenCoordinates({
+      from: {
+        latitude: Number(gym.latitude),
+        longitude: Number(gym.longitude),
+      },
+      to: { latitude: userLatitude, longitude: userLongitude },
+    });
+
+    const MAX_DISTANCE_IN_KILOMETERS = 0.1;
+
+    if (distance > MAX_DISTANCE_IN_KILOMETERS) {
+      throw new CheckInDistanceError();
+    }
+
+    const checkInOnSameDay = await this.checkInRepository.findByUserIdOnDate({
+      userId,
+      date: new Date(),
+    });
+
+    if (checkInOnSameDay) {
+      throw new CheckInOnSameDayError();
+    }
+
     const checkIn = await this.checkInRepository.create({
       gym_id: gymId,
       user_id: userId,
